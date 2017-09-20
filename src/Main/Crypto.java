6112265eadb3e18ad53df603683b9dd7ad3359ba
Sourish Banerjee <sourish.banerjee2015@vit.ac.in>
2017-09-20 10:16:52 +0530

    Update Final String

--- src/Main/Crypto.java
+++ src/Main/Crypto.java
@@ -23,7 +23,7 @@ public class Crypto {
                     newAsciiValue = newAsciiValue - 26;
             }
 
-            encodedString += (char) newAsciiValue + "";
+            encodedString += (char) newAsciiValue + "_";
         }
 
         return encodedString;
